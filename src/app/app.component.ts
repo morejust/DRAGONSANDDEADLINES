0fcf86ddc63f1322c4fef9583686dac958c42968
bob <bob@>
2019-04-27 23:11:10 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import * as am4core from '@amcharts/amcharts4/core';
+import {Section} from './main/main.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'HrInsights';
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+  private chart: am4charts.XYChart;
+  folders: Section[] = [
+    {
+      name: 'Сам себе безопасник I',
+      description: 'Пройти проверку службы безопасности с первого раза',
+    },
+    {
+      name: 'Сам себе безопасник I',
+      description: 'Пройти проверку службы безопасности с первого раза',
+    },
+    {
+      name: 'Сам себе безопасник I',
+      description: 'Пройти проверку службы безопасности с первого раза',
+    },
+    {
+      name: 'Сам себе безопасник I',
+      description: 'Пройти проверку службы безопасности с первого раза',
+    }];
+
+  constructor(private zone: NgZone) { }
+
+  ngAfterViewInit() {
+    this.zone.runOutsideAngular(() => {
+      const chart = am4core.create('chartdiv', am4charts.RadarChart);
+
+      chart.data = [
+        {
+          country: 'Python',
+          litres: 501
+        }, {
+          country: 'Node.js',
+          litres: 301
+        }, {
+          country: 'Bash',
+          litres: 266
+        }, {
+          country: 'Angular',
+          litres: 400
+        }, {
+          country: 'React',
+          litres: 139
+        }, {
+          country: 'Docker',
+          litres: 336
+        }, {
+          country: 'CI/CD',
+          litres: 290
+        }
+      ];
+
+      /* Create axes */
+      const categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis<any>());
+      categoryAxis.dataFields.category = 'country';
+
+      const valueAxis = chart.yAxes.push(new am4charts.ValueAxis<any>());
+
+      /* Create and configure series */
+      const series = chart.series.push(new am4charts.RadarSeries());
+      series.dataFields.valueY = 'litres';
+      series.dataFields.categoryX = 'country';
+      series.name = 'Sales';
+      series.strokeWidth = 3;
+
+      this.chart = chart;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.zone.runOutsideAngular(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
@@ -25,7 +24,6 @@ import { MainComponent } from './main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
